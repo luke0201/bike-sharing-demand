9a2aac3a91e5e0260e6dc27880d70352aec743b6
Luke Lee <luke0201@naver.com>
2021-05-18 16:26:01 +0900

    Fix typos

--- bike-sharing-demand.py
+++ bike-sharing-demand.py
@@ -4,7 +4,6 @@ import sys
 
 import numpy as np
 import pandas as pd
-
 from sklearn.pipeline import Pipeline
 from sklearn.compose import ColumnTransformer
 from sklearn.preprocessing import OneHotEncoder
